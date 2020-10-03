0b30795c8872bf0b89821f48bf8d6975b9c6a28b
Jun <heartenc@126.com>
2020-10-03 22:17:57 +0800

    update 2020 10 3

--- Case/case_test/Project_exm/1_case_pass_exm/1_test_通过用例示例.py
+++ Case/case_test/Project_exm/1_case_pass_exm/1_test_通过用例示例.py
@@ -12,6 +12,7 @@ public.make_dir('test_path', 'test_path', '')
 public.make_file('test_path/test_path', 'file_name_1', 'test_word', '')
 public.find('test_path/test_path', 'file_name_1', '')
 public.input_file('test_path/test_path', 'file_name_1', 'test_word', '')
+public.wait_seconds('5')
 
 # ============================
 public.teardown()
